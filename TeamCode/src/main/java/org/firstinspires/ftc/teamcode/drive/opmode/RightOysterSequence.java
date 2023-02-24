ae36a76dde5ce5f24661d0a1e4d6d426e4e1cb33
Quantum16505 <quantum16505@gmail.com>
2023-02-24 13:45:50 -0600

    reduced forward yet again in zone 3 - right

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/RightOysterSequence.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/RightOysterSequence.java
@@ -221,7 +221,7 @@ public class RightOysterSequence extends LinearOpMode {
                 // Right telemetry goes here
                 TrajectorySequence trajRight = drive.trajectorySequenceBuilder(trajSeq.end())
                         .turn(Math.toRadians(90))
-                        .forward(32)
+                        .forward(30)
                         .build();
                 drive.followTrajectorySequence(trajRight);
 
